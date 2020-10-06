d20a0b7f0dbe34865211f8652621956139566b37
Collin Ballou <BlueEagle@users.noreply.github.com>
2020-10-06 10:19:52 -0600

    done

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -138,7 +138,7 @@ class Graph:
                 visited.add(v)
 
                 for neighbor in self.get_neighbors(v):
-                    q.enqueue(path + [neighbor])
+                    q.push(path + [neighbor])
 
     def dfs_recursive(self, starting_vertex, destination_vertex, visited = set()):
         """
@@ -208,7 +208,7 @@ if __name__ == '__main__':
         1, 2, 4, 3, 7, 6, 5
         1, 2, 4, 3, 7, 5, 6
     '''
-    # graph.bft(1)
+    graph.bft(1)
 
     '''
     Valid DFT paths:
@@ -217,19 +217,19 @@ if __name__ == '__main__':
         1, 2, 4, 7, 6, 3, 5 ✔
         1, 2, 4, 6, 3, 5, 7
     '''
-    # graph.dft(1)
-    # graph.dft_recursive(1)
+    graph.dft(1)
+    graph.dft_recursive(1)
 
     '''
     Valid BFS path:
         [1, 2, 4, 6]
     '''
-    # print(graph.bfs(1, 6))
+    print(graph.bfs(1, 6))
 
     '''
     Valid DFS paths:
         [1, 2, 4, 6]
         [1, 2, 4, 7, 6]
     '''
-    # print(graph.dfs(1, 6))
+    print(graph.dfs(1, 6))
     print(graph.dfs_recursive(1, 6))
